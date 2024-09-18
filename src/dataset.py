f1ce7e9d389d11c22ed74b4f033f647687a62d45
abiolaks <kingsleylawani@gmail.com>
2024-09-18 15:09:55 +0100

    added dataset and jupyter notebooks

---
+++ src/dataset.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+# 2. Creating a DataFrame
+data = {
+    "Name": ["Alice", "Bob", "Charles", "David"],
+    "Age": [24, 27, 22, 23],
+    "City": ["New York", "Los Angeles", "Chicago", "Houston"],
+}
+df = pd.DataFrame(data)
